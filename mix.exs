4694d9a035f4e0a7cab0dc1c4ae099698065b8ae
jad <j.tarabay@eweev.com>
2018-03-06 17:26:46 +0200

    0.1.10

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExSbapi.Mixfile do
   use Mix.Project
 
-  @version "0.1.9"
+  @version "0.1.10"
   @name "shopbuilder_api"
   @maintainers ["Jad Tarabay", "Julien Fayad"]
   @url "https://github.com/Eweev/ex_sbapi"
